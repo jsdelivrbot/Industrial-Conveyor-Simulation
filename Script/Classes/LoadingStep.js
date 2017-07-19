c421ce4736b0608627af8a7eda1541d5214420a6
Guilherme Rossato <guilhermrossato@gmail.com>
2017-07-18 23:16:33 -0300

    Removed Loading Step File
    Relocated Loading Step class to Load.js because it got too small

--- Script/Classes/LoadingStep.js
+++
@@ -1,7 +0,0 @@
-function LoadingStep(label, type, execute) {
-	if (type !== "sync" && type !== "assync")
-		throw new Error("Unhandled input");
-	this.label = label;
-	this.type = type;
-	this.execute = execute;
-}
