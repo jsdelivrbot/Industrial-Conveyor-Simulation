a38ca27884756de859281aa3e53965fb21ce2154
Guilherme Rossato <guilhermrossato@gmail.com>
2017-07-18 23:28:45 -0300

    Added a better error display

--- Script/Classes/LoaderInterface.js
+++ Script/Classes/LoaderInterface.js
@@ -10,12 +10,17 @@ LoaderInterface.prototype = {
 	},
 	error: function(title, stack) {
 		this.headers[0].innerText = title;
-		this.headers[1].innerHTML = `<div style="font-size:0.5em">${stack.split("\n").join("<br>")}</div>`;
-		this.headers[2].innerHTML = "";
+		var lines = stack.split("\n");
+		if (lines.length > 0) {
+			lines[0] = `<span style="font-weight:bold">${lines[0]}</span>`;
+		}
+		this.headers[1].innerHTML = `<div style="margin-top:10px;line-height:1.5em; font-size:0.5em; white-space: pre; text-align:left;">${lines.join("<br>")}</div>`;
+		this.headers[2].innerHTML = `<a href="#" onclick="Load()">Continue anyways</a>`;
 	},
 	update: function(text, percent) {
-		if (this.headers.length === 0)
+		if (this.headers.length === 0) {
 			return
+		}
 		text.split('\n').some((line, headerIndex) => {
 			if (headerIndex+1 < this.headers.length) {
 				this.headers[headerIndex+1].innerText = line;
@@ -33,5 +38,6 @@ LoaderInterface.prototype = {
 			this.error("HTML Document Error", (new Error(`Unable to find "mask" on the document`)).stack);
 		}
 		this.headers[1].innerText = "Done";
+		this.headers[2].innerText = "100%";
 	}
 }
