e03148dbec45e2c3d2798c17ff029efb1b3e8f03
Guilherme Rossato <guilhermrossato@gmail.com>
2017-07-18 23:15:57 -0300

    Added a Test Object
    To test rendering

--- Script/Classes/WorldHandler.js
+++ Script/Classes/WorldHandler.js
@@ -1,5 +1,5 @@
 function WorldHandler() {
-	
+	this.count = 0;
 }
 
 WorldHandler.prototype = {
@@ -7,10 +7,21 @@ WorldHandler.prototype = {
 	setImageList: function(imageList) {
 		this.imageList = imageList;
 	},
-	initWebWorkers: function() {
-		
+	collect: function(gatherer) {
+		this.scene = gatherer.scene || (()=>{throw new Error("Couldn't retrieve scene")})();
+	},
+	initWebWorkers: function(gatherer, seed) {
+
 	},
 	update: function(delta) {
-		
+		if (this.count === 0) {
+			var material = new THREE.MeshLambertMaterial({
+				color: 0x222222
+			});
+			this.a = new THREE.Mesh(new THREE.CubeGeometry(1,0.5,1), material);
+			this.scene.add(this.a);
+		}
+		this.a.rotation.set(this.a.rotation.x-0.002,0,this.a.rotation.z+0.01);
+		this.count ++;
 	}
 }
